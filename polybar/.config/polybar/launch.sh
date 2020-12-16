d644fecd426a0b29c40780cd5e6604ca78dbffcd
Vanni Piana <sonny.mac70@gmail.com>
2020-12-16 20:35:39 +1000

    one display setup

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,7 +8,7 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar DP1 >>/tmp/polybar1.log 2>&1 & disown
-polybar HDMI1 >>/tmp/polybar22log 2>&1 & disown
+#polybar HDMI1 >>/tmp/polybar22log 2>&1 & disown
 
 
 echo "Bars launched..."
